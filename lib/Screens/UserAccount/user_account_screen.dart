c7e0f8f87f91398a8c38d377b18829828a6648d3
Flavia Penim <flaviapenim@gmail.com>
2021-07-09 18:23:42 +0100

    Implement base account Screen

--- lib/Screens/UserAccount/user_account_screen.dart
+++ lib/Screens/UserAccount/user_account_screen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hat/Screens/Login/login_screen.dart';
+import 'package:flutter_hat/components/rounded_button.dart';
 import 'package:flutter_hat/constants.dart';
 
 class UserAccountScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
@@ -13,7 +16,57 @@ class UserAccountScreen extends StatelessWidget {
           style: Theme.of(context).textTheme.headline6,
         ),
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: size.width * 0.2,
+                    height: size.height * 0.1,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage('assets/icons/user.png'),
+                      ),
+                      shape: BoxShape.circle,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  '[username]',
+                  style: Theme.of(context).textTheme.headline5,
+                ),
+              ],
+            ),
+            SizedBox(
+              width: size.width,
+              height: size.height * 0.5,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                RoundedButton(
+                  text: 'LOG OUT',
+                  press: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()),
+                    );
+                  },
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
